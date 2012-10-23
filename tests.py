2a310e9fd3b0624070d282d958d56e7e3a446b3b
Daniel Fairhead <danthedeckie@gmail.com>
2012-10-23 22:56:11 +0100

    a bunch more unit tests.

--- tests.py
+++ tests.py
@@ -18,7 +18,7 @@ from DictLiteStore import DictLiteStore
 
 def store_and_get(d):
     ''' stores a dictionary in a :memory: / default DictLiteStore,
-        and returns the first row of querying that table. '''
+        and returns all rows in that table '''
 
     with DictLiteStore() as s:
         s.store(d)
@@ -26,12 +26,12 @@ def store_and_get(d):
 
         assert len(dd) == 1
 
-        return dd[0]
+        return dd
 
 def store_and_compare(original, result=False):
     ''' stores a dictionary, retrieves it again,
         and compares with an expected result. '''
-    retrieved = store_and_get(original)
+    retrieved = store_and_get(original)[0]
 
     if not result:
         assert retrieved == original
@@ -45,46 +45,46 @@ def store_and_compare(original, result=False):
 #########################################
 
 def test_text_only():
-    a = {'row1':'data1', 'row2':'data2'}
+    a = {'col1':'data1', 'col2':'data2'}
     store_and_compare(a)
 
 def test_unicode_text():
-    a = {'rowενα':u'πραγμα', 'rowδυο':'älles güt'}
+    a = {'colενα':u'πραγμα', 'colδυο':'älles güt'}
     store_and_compare(a)
 
 def test_int():
-    a = {'row1': 42}
+    a = {'col1': 42}
     store_and_compare(a)
 
 def test_float():
-    a = {'row1': 3.14 }
+    a = {'col1': 3.14 }
     store_and_compare(a)
 
 def test_list():
-    a = {'row1':['this','is','a','list']}
+    a = {'col1':['this','is','a','list']}
     store_and_compare(a)
 
 def test_unicode_list():
-    a = {'row1':['αὐτο','είναι','εναν', 'unicode', 'list']}
+    a = {'col1':['αὐτο','είναι','εναν', 'unicode', 'list']}
     store_and_compare(a)
 
 def test_dict():
-    a = {'row1':{'subdict':'value'}}
+    a = {'col1':{'subdict':'value'}}
     store_and_compare(a)
 
 def test_function():
-    a = {'row1':'should work', 'row2': map}
-    b = {'row1':'should work', 'row2': '<built-in function map>'}
+    a = {'col1':'should work', 'col2': map}
+    b = {'col1':'should work', 'col2': '<built-in function map>'}
     store_and_compare(a, b)
 
 def test_class():
 
     c = object()
 
-    a = {'row1': c }
+    a = {'col1': c }
 
-    b = store_and_get(a)
-    assert b['row1'].startswith('<object object at')
+    b = store_and_get(a)[0]
+    assert b['col1'].startswith('<object object at')
 
 ##########################################
 #
@@ -92,3 +92,134 @@ def test_class():
 # let's try doing more interesting tests.
 #
 ##########################################
+
+# some generic data:
+ROW1 = {'col1':'data1', 'col2':'data2'}
+ROW2 = {'col3':'data3', 'col4':'data4'}
+# a generic update:
+UPDATE1 = {'col1':'UPDATED'}
+# a generic WHERE clause, which matches ROW1
+GOODWHERE = ('col1','==','data1')
+# a generic WHERE clause, which doesn't match
+BADWHERE = ('col1','==','bogus')
+
+# a couple of boilerplate reduction functions:
+
+def copy_change(original, updates):
+    # I suspect there is a standard library function for this.
+    new = original.copy()
+    new.update(updates)
+    return new
+
+def send_update_compare(data, updates, post_data):
+    ''' store some data, run some updates, compare the result. '''
+    with DictLiteStore() as s:
+        for d in data:
+            s.store(d)
+        for u in updates:
+            s.update(u)
+
+        assert s.get() == list(post_data)
+
+# And the update tests:
+
+def test_multiple_rows_same_columns():
+    send_update_compare(
+        (ROW1, ROW1),
+        (),
+        (ROW1, ROW1))
+
+def test_rows_with_different_columns():
+
+    with DictLiteStore() as s:
+        s.store(ROW1)
+        s.store(ROW2)
+
+        c = s.get()
+        assert c[0] == ROW1
+        assert c[1] == ROW2
+
+
+def test_update_all_rows_with_one_entry():
+
+    post_update = copy_change(ROW1, UPDATE1)
+
+    with DictLiteStore() as s:
+        s.store(ROW1)
+        s.update(UPDATE1)
+        from_db = s.get()
+
+        assert from_db[0] == post_update
+
+
+def test_update_all_rows_with_multiple_entries():
+
+    post_update_a = copy_change(ROW1, UPDATE1)
+    post_update_b = copy_change(ROW2, UPDATE1)
+
+    with DictLiteStore() as s:
+        s.store(ROW1)
+        s.store(ROW2)
+        s.update(UPDATE1)
+        from_db = s.get()
+
+        assert from_db[0] == post_update_a
+        assert from_db[1] == post_update_b
+
+
+def test_update_single_row():
+
+    post_update_a = copy_change(ROW1, UPDATE1)
+
+    with DictLiteStore() as s:
+        s.store(ROW1)
+        s.store(ROW2)
+        s.update(UPDATE1, False, GOODWHERE)
+        from_db = s.get()
+
+        assert from_db[0] == post_update_a
+        assert from_db[1] == ROW2
+
+def test_update_fallbackto_insert():
+
+    with DictLiteStore() as s:
+        s.store(ROW1)
+        s.update(UPDATE1, True, BADWHERE)
+        from_db = s.get()
+
+        assert from_db[0] == ROW1
+        assert from_db[1] == UPDATE1
+
+
+def test_update_fallbackto_nothing():
+
+    with DictLiteStore() as s:
+        s.store(ROW1)
+        s.update(UPDATE1, False, BADWHERE)
+        from_db = s.get()
+
+        assert len(from_db) == 1
+        assert from_db[0] == ROW1
+
+def test_update_empty_table():
+    with DictLiteStore() as s:
+        s.update(UPDATE1, False, BADWHERE)
+        from_db = s.get()
+
+        assert from_db == []
+
+def test_update_empty_table_fallbackto_insert():
+    with DictLiteStore() as s:
+        s.update(UPDATE1, True, BADWHERE)
+        from_db = s.get()
+        assert from_db == [UPDATE1]
+
+
+
+
+
+# TODO:
+# - test writing to a file.
+# - test a different table name.
+# - test deletion
+# - test custom SQL schema databases.
