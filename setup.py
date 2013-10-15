e91147eaa8f394c083ca2c5c591e10e7ad5ce7fe
Daniel Fairhead <daniel.fairhead@om.org>
2013-10-15 15:44:56 +0100

    Have become more PyPI compatible...

--- setup.py
+++ setup.py
@@ -1,19 +1,22 @@
 from distutils.core import setup
+from dictlitestore import __version__
+
 setup(
     name = 'DictLiteStore',
-    packages = ['DictLiteStore'],
-    version = '0.9.1',
+    py_modules = ['dictlitestore'],
+    version = __version__,
     description = 'A (Dynamic-Schema) dict -> sqlite3 library',
+    long_description=open('README.rst','r').read(),
     author = 'Daniel Fairhead',
     author_email = 'danthedeckie@gmail.com',
     url = 'https://github.com/danthedeckie/DictLiteStore',
-    download_url = 'https://github.com/danthedeckie/DictLiteStore/tarball/0.9.1',
+    download_url = 'https://github.com/danthedeckie/DictLiteStore/tarball/' + __version__,
     keywords = ['json', 'dict', 'persistance', 'sqlite', 'dynamic schema'],
     classifiers = ['Development Status :: 4 - Beta',
                    'Intended Audience :: Developers',
-                   'Licence :: OSI Approved :: GNU General Purpose Public Licence v3 (GPLv3)',
+                   'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
                    'Topic :: Database',
-                   'Programming Language :: Python 2.7',
+                   'Programming Language :: Python :: 2.7',
 
                   ],
     )
